20c5dce40b6a8fee43de7c59aec2f1d1b2f5c9e8
Nabiha Khan <nabiha.khan@me.com>
2021-09-03 13:46:01 -0700

    The FULL APP

--- README.md
+++ README.md
@@ -1,29 +1,61 @@
-# Personal_Portfolio
+# React Portfolio [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-**Requirements**
+## Description
 
-* Submit an updated portfolio featuring Project 1 and two exemplary homework assignments.
-* Submit an updated GitHub profile with pinned repositories featuring Project 1 and two exemplary assignments.
-* Submit an updated resume
-* Submit an updated LinkedIn profile
+The purpose of this project was to create a portfolio into a single page application using React.
 
-**Mock-Up**
+## Table of Contents
 
-![](./example.gif)
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [Contributing](#Contributing)
+4. [License](#License)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
 
-**Challenges | Comments**
+## Installation
 
-* I used a tutorial to make this portfolio. Actually, I used several tutorials.
-* I did not have a hard time coding this portfolio. However, I lack artistic talent, 
-and thus used many tutorial to graps a design.
-* In an ideal world, I would like the user to be able to send me their information. I technically could store it 
-in local storage, but I dont want user information to be compromised. So I think once I purchase a URL, I will set it up.
-* I need more projects that put me out against my peers. So I will try to work on side projects when I can.
-* ALSO, I cannot update my LinkedIn and resume at the moment. I do not want my current employeers to know I am taking this 
-program. They will get anxious learning I will be leaving soon, and that could cause a rough couple of months. 
+The deployed application requires no installation. Developers working with this code must have Node.js installed and run `npm i` to install dependencies. Then to run the app, do `npm start`.
 
-**Links**
+## Usage
 
-*Live URL* : https://nkhan14.github.io/
+Potential employers can view my portfolio, including a recent highlighted project, with links to the source code and deployed applications. Employers can contact me directly, view my resume, and review my skills on this single page application.
 
-*GitHub URL* : https://github.com/nkhan14/Personal_Portfolio
+![A demonstration of the README Generator using sample inputs](/public/assets/images/DeployedScreenshot.png)
+
+## Contributing
+
+This project was created independently.
+
+## License
+
+MIT License
+
+Copyright (c) 2021 Nabiha Haleema Khan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Tests
+
+There are currently no tests for this project.
+
+## Questions
+
+Reach out to the developer at nabiha.khan@me.com if you have questions about this project. Many different developers references have been commented on the code.
---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/nkhan14",
+    "name": "Nabiha Khan",
+    "role": "Full-Stack Developer and Field Applications Scientist",
+    "linkedinId":"https://www.linkedin.com/in/nabihahkhan/",
+    "roleDescription": "I like integrating two disciplines - science and program developing.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nabihahkhan/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/nkhan14",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/_nabihakhan",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am hard working and compassionate, but more than anything I am utterly committed to a career in cancer biology and software developing. I understand that this is a career with many demands, both emotional and physical, but I know that I possess the energy, determination and stamina to withstand these difficulties and to become a useful member of any team I join. I currently am a Field Application Scientist responsible for performing training and supporting customers.",
+    "address":"Pleasanton, CA",
+    "website":"https://nkhan14.github.io/",
+    "education":[
+      {
+        "UniversityName":"Northwestern University",
+        "specialization":"Full-Stack Web Development Program",
+        "MonthOfPassing":"Sept",
+        "YearOfPassing":"2021",
+        "Grade":"A+"
+      },
+      {
+        "UniversityName":"University of Illinois COM",
+        "specialization":"Masters in Medical Biotechnology",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Illinois Institute of Technology",
+        "specialization":"Bachelors in Biochemistry",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"S2 Genomics",
+        "specialization":"Lead Field Applications Scientist",
+        // "MonthOfLeaving":"Current",
+        // "YearOfLeaving":"2018",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Northwestern University Feinberg",
+        "specialization":"Research Associate II",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2021",
+        "Achievements":"NGS Resident Expert"
+      }
+    ],
+    "skillsDescription":"Coding Skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project 1",
+        "description":"Covid Catcher",
+        "imgurl":"/images/portfolio/coronavirus-2.jpg"
+      },
+      {
+        "name":"Project 2",
+        "description":"Make Trivia Great Again",
+        "imgurl":"/images/portfolio/trivia-logo.png"
+      },
+      {
+        "name":"Project 3",
+        "description":"MERN Your Books",  
+        "imgurl":"/images/portfolio/book-homepage-image.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"I have known Nabiha for more than an year now. I was her Teaching Assistant at University of Illinois, College of Medicine at Rockford for the Molecular Biology laboratory course. She is a smart learner who is skilled in molecular biology techniques like Cloning, PCR, Transfection. Nabiha is also very dedicated and sincere to her work. She is going to be a valuable addition to all her future work places. I wish Nabiha the best and I am sure she will grow as a successful researcher.",
+        "name":"Angela Lincy Prem Anthony Samy"
+      },
+      {
+        "description":"I have had the pleasure of knowing Nabiha for the past two years. She is a great leader and has very good managerial skills. Nabiha is a very insightful, communicative, enthusiastic, well-read and widely-connected person who has a broad yet in-depth knowledge in the field of oncology and immunology. Nabiha is good at spotting communication and is always upbeat and cheerful which makes her a multiplier making everyone around her better, so knowing her has always been an added benefit. She is quite approachable and always finds time to have a discussion to share her knowledge and experience in her field of work. I have often been guided by Nabiha’s external advice on my extracurricular activities. She is an excellent speaker with an exceptional presentation skill. I would like to say that Nabiha would be an extremely valuable asset to any organization. I wish Nabiha all the very best!",
+        "name":"Nikhil Karthik"
+      }
+    ]
+  }
+  
+  export default resumeData
